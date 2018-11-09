3a48992cd41ee556bd597e131d3d5f9156905f4c
Mateo <be17b021@technikum-wien.at>
2018-11-09 18:41:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mvasc/imm_app/Start.java
@@ -0,0 +1,16 @@
+package com.example.mvasc.imm_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
+private void PullUp(){
+    
+}
